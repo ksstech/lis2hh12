fb06423db344754986cf13aaed1172e2225e6f74
André Maree <ammaree@users.noreply.github.com>
2022-06-13 14:42:13 +0200

    Change to use CRLF on all messages

--- lis2hh12.c
+++ lis2hh12.c
@@ -95,7 +95,7 @@ int	lis2hh12ReadHdlrAccel(epw_t * psEWP) {
 	vCV_SetValue(&table_work[URI_LIS2HH12_Y].var, X64);
 	X64.x32[0].f32 = (float) sLIS2HH12.Reg.u16OUT_Z * X64.x32[1].f32;
 	vCV_SetValue(&table_work[URI_LIS2HH12_Z].var, X64);
-	IF_P(debugCONVERT, "lis2hh12  [ %-`B ]\n", 7, &sLIS2HH12.Reg.STATUS);
+	IF_P(debugCONVERT, "lis2hh12  [ %-`B ]\r\n", 7, &sLIS2HH12.Reg.STATUS);
 	if (ioB1GET(ioLIS2HH12)) {
 		if (sLIS2HH12.Reg.status.zyxor)
 			P("LIS2HH12 ZYX overrun");
@@ -130,7 +130,7 @@ int	lis2hh12ConfigMode (struct rule_t * psR, int Xcur, int Xmax, int EI) {
 	int dur = psR->para.x32[AI][1].i32;
 	int odr = psR->para.x32[AI][2].i32;
 	int hr = psR->para.x32[AI][3].i32;
-	IF_P(debugTRACK && ioB1GET(ioMode), "lis2hh12: Xcur=%d Xmax=%d ths=%d dur=%d odr=%d hr=%d\n", Xcur, Xmax, ths, dur, odr, hr);
+	IF_P(debugTRACK && ioB1GET(ioMode), "lis2hh12: Xcur=%d Xmax=%d ths=%d dur=%d odr=%d hr=%d\r\n", Xcur, Xmax, ths, dur, odr, hr);
 
 	if (OUTSIDE(lis2hh12M_NORMAL, mode, lis2hh12M_STREAM, int) ||
 		OUTSIDE(0, ths, 127, int) ||
@@ -145,7 +145,7 @@ int	lis2hh12ConfigMode (struct rule_t * psR, int Xcur, int Xmax, int EI) {
 		sLIS2HH12.Reg.ctrl1.hr = hr;
 		sLIS2HH12.Reg.ctrl1.odr = odr;
 		lis2hh12WriteReg(lis2hh12CTRL1, sLIS2HH12.Reg.CTRL1);
-		IF_P(debugTRACK && ioB1GET(ioMode), "lis2hh12: THS=0x%02X  DUR=0x%02X  CTRL1=ox%02X\n",
+		IF_P(debugTRACK && ioB1GET(ioMode), "lis2hh12: THS=0x%02X  DUR=0x%02X  CTRL1=ox%02X\r\n",
 				sLIS2HH12.Reg.ACT_THS,sLIS2HH12.Reg.ACT_DUR, sLIS2HH12.Reg.CTRL1);
 	} while (++Xcur < Xmax);
 	return erSUCCESS;
@@ -223,20 +223,20 @@ int	lis2hh12Diags(i2c_di_t * psI2C_DI) { return erSUCCESS; }
 
 void lis2hh12ReportAll(void) {
 	halI2C_DeviceReport(sLIS2HH12.psI2C);
-	P("\tACT_THS: 0x%02X (%dmg) \n", sLIS2HH12.Reg.ACT_THS, sLIS2HH12.Reg.ACT_THS*(fs_scale[sLIS2HH12.Reg.ctrl4.fs]/128));
-	P("\tACT_DUR: 0x%02X (%ds) \n", sLIS2HH12.Reg.ACT_DUR, sLIS2HH12.Reg.ACT_DUR);
-	P("\tCTRL1: 0x%02X  hr=%d  odr=%d (%DHz) bdu=%d  Zen=%d  Yen=%d  Xen=%d\n", sLIS2HH12.Reg.CTRL1,
+	P("\tACT_THS: 0x%02X (%dmg) \r\n", sLIS2HH12.Reg.ACT_THS, sLIS2HH12.Reg.ACT_THS*(fs_scale[sLIS2HH12.Reg.ctrl4.fs]/128));
+	P("\tACT_DUR: 0x%02X (%ds) \r\n", sLIS2HH12.Reg.ACT_DUR, sLIS2HH12.Reg.ACT_DUR);
+	P("\tCTRL1: 0x%02X  hr=%d  odr=%d (%DHz) bdu=%d  Zen=%d  Yen=%d  Xen=%d\r\n", sLIS2HH12.Reg.CTRL1,
 		sLIS2HH12.Reg.ctrl1.hr, sLIS2HH12.Reg.ctrl1.odr, odr_scale[sLIS2HH12.Reg.ctrl1.odr],
 		sLIS2HH12.Reg.ctrl1.bdu,
 		sLIS2HH12.Reg.ctrl1.zen, sLIS2HH12.Reg.ctrl1.yen, sLIS2HH12.Reg.ctrl1.xen);
-	P("\tCTRL4: 0x%02X  bw=%d  fs=%d (%dG)  bws_odr=%d  IAinc=%d  I2Cen=%d  sim=%d\n", sLIS2HH12.Reg.CTRL4,
+	P("\tCTRL4: 0x%02X  bw=%d  fs=%d (%dG)  bws_odr=%d  IAinc=%d  I2Cen=%d  sim=%d\r\n", sLIS2HH12.Reg.CTRL4,
 		sLIS2HH12.Reg.ctrl4.bw, sLIS2HH12.Reg.ctrl4.fs, fs_scale[sLIS2HH12.Reg.ctrl4.fs]/1000,
 		sLIS2HH12.Reg.ctrl4.bw_scale_odr,
 		sLIS2HH12.Reg.ctrl4.if_add_inc,
 		sLIS2HH12.Reg.ctrl4.i2c_enable,
 		sLIS2HH12.Reg.ctrl4.sim);
-	P("\tCTRL5: 0x%02X  debug=%d  reset=%d  dec=%d  st=%d  HLactive=%d  pp_od=%d\n", sLIS2HH12.Reg.CTRL5,
+	P("\tCTRL5: 0x%02X  debug=%d  reset=%d  dec=%d  st=%d  HLactive=%d  pp_od=%d\r\n", sLIS2HH12.Reg.CTRL5,
 		sLIS2HH12.Reg.ctrl5.debug, sLIS2HH12.Reg.ctrl5.soft_reset, sLIS2HH12.Reg.ctrl5.dec,
 		sLIS2HH12.Reg.ctrl5.st, sLIS2HH12.Reg.ctrl5.h_lactive, sLIS2HH12.Reg.ctrl5.pp_od);
-//	P("I1: 0x%02x  I2: 0x%02x\n", sLIS2HH12.Reg.IG_SRC1, sLIS2HH12.Reg.IG_SRC2);
+//	P("I1: 0x%02x  I2: 0x%02x\r\n", sLIS2HH12.Reg.IG_SRC1, sLIS2HH12.Reg.IG_SRC2);
 }
